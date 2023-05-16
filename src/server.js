f8e196d5234f56d7df4c8a88482e20ee0ab06048
aminahbusiness22 <133093229+aminahbusiness22@users.noreply.github.com>
2023-05-16 21:09:27 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
+const cors = require('cors');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: "0.0.0.0",
+    "routes": {
+      "cors": true
+  }
   });
 
   // server.route({
